31e4b58c3dbb839e470ec50020c67560a6a2557c
ziko387 <mwangiian973@gmail.com>
2025-07-08 09:26:53 +0300

    added new python file

--- test.pt.py
+++ test.pt.py
@@ -1,7 +1,23 @@
 age= int(input("enter your age"))
 
+
 if age>=5 and age<=10:
     print("allowed to drink juice but not an energy drink")
+    print("types of juice")
+    print("1.fanta")
+    print("2.sprite")
+    print("3.delamonte")
+    print("4.cocacola")
+    choice= input("select your favorite juice:")
+    juice={
+        "1":"fanta",
+        "2":"sprite",
+        "3":"delamonte",
+        "4":"cocacola"
+
+    }
+    select_juice= juice.get(choice)
+    print(f"you have selected:{select_juice} sucessfuly")
 elif age>=11 and age<=17:
     print("allowed to drink energy drink but not wine")
 elif age>=18 and age<=25:
